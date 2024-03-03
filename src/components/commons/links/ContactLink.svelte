9be8430084f7c925cbc65262a2ce818a331c4939
ckw28502 <ckw28502@gmail.com>
2024-03-03 15:54:11 +0100

    feat: add footer

---
+++ src/components/commons/links/ContactLink.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type ContactModel from "../../../models/ContactModel";
+
+    export let contact: ContactModel;
+
+</script>
+
+<a href={contact.getUrl()} target="_blank" class="w-1/4 p-4 flex items-center justify-center ">
+    <i class="fab text-3xl mr-2 fa-{contact.getName()}" />
+</a>
---
+++ src/services/ContactService.ts
@@ -0,0 +1,8 @@
+import ContactData from "../data/ContactData.json"
+import ContactModel from "../models/ContactModel";
+
+function convertContactDataToContactModel(): ContactModel[] {
+    return ContactData.map(contact => new ContactModel(contact.name, contact.url))
+}
+
+export default {convertContactDataToContactModel}
